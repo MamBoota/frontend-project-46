0687463f73494d608848434048a264c669380739
MamBoota <deniskostyuk.93@yandex.ru>
2024-05-13 21:52:05 +0300

    Step 2, done.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]',  'output format')
   .helpOption('-h, --help', 'output usage information')
 
 
