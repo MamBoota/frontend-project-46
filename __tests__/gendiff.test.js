a2ac378d3ae8c69a6e361c893d5a76b18930d06f
MamBoota <deniskostyuk.93@yandex.ru>
2024-09-12 00:48:39 +0300

    Fix error build.

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,5 @@
 import genDiff from '../src/index.js';
-import path from '../src/index.js';
+import path from 'path';
 import expectedStylish from '../__fixtures__/expectedStylish_file.js';
 import expectedPlain from '../__fixtures__/expectedPlain_file.js';
 import expectedJSON from '../__fixtures__/expectedJSON_file.js';
