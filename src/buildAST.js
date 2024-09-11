bc8989d510df8b6fd12896fa2c0f76bb50a92a98
MamBoota <deniskostyuk.93@yandex.ru>
2024-09-12 01:01:44 +0300

    Fix code climate.

--- src/buildAST.js
+++ src/buildAST.js
@@ -7,39 +7,18 @@ const buildAST = (data1, data2) => {
 
   const children = sortedKeys.map((key) => {
     if (!_.has(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: data2[key],
-      };
+      return { type: 'added', key, value: data2[key] };
     }
     if (!_.has(data2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value: data1[key],
-      };
+      return { type: 'removed', key, value: data1[key] };
     }
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return {
-        type: 'nested',
-        key,
-        children: buildAST(data1[key], data2[key]),
-      };
+      return { type: 'nested', key, children: buildAST(data1[key], data2[key]) };
     }
     if (_.isEqual(data1[key], data2[key])) {
-      return {
-        type: 'unchanged',
-        key,
-        value: data1[key],
-      };
+      return { type: 'unchanged', key, value: data1[key] };
     }
-    return {
-      type: 'changed',
-      key,
-      oldValue: data1[key],
-      newValue: data2[key],
-    };
+    return { type: 'changed', key, oldValue: data1[key], newValue: data2[key] };
   });
   return children;
 };
